7d53663b2332c16244e78a754962931e5d19b45b
Andrés Palacio <andres.palacio@utp.edu.co>
2024-05-16 17:59:17 -0500

    Authentication and Metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,24 @@
-export default function Customers() {
+import { Metadata } from 'next';
+import { fetchCustomers } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Customers() {
+  const customers = await fetchCustomers();
+
   return (
-    <div>Customer Page</div>
-  )
+    <div>
+      <h1 className='text-3xl bold'>Customers</h1>
+      <ul>
+        {customers.map((customer) => (
+          <li key={customer.id}>
+            <a href={`/customers/${customer.id}`}>{customer.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+    // <p>{Object.keys(customers)}</p>
+  );
 }
